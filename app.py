443508ad5891d5398fba5917a25d75d61887dc2c
Rajat Latwal <rajatlatwal7@gmail.com>
2025-07-23 10:20:25 +0530

    Final-Commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+from PIL import Image # This is for image Display 
+import google.generativeai as genai
+
+# Configure the Key and Initiate the Model
+genai.configure(api_key=os.getenv('GOOGLE-API-KEY'))
+model=genai.GenerativeModel('gemini-1.5-flash')
+
+# Create the front end of the Page
+st.header('🖼️ AI Image-to-Text Campaign Builder 📈',divider=True)
+st.subheader('╰┈➤ Enter the Prompt',divider=True)
+prompt=st.text_input(label='Enter Details',max_chars=10000)
+uploaded_image=st.file_uploader('Upload the Image',type=['jpg','jpeg','png'])
+
+# Image Display
+image=''
+if uploaded_image is not None:
+    image=Image.open(uploaded_image)
+    st.image(image,use_container_width=True)
+
+def get_llm_response(prompt,image):
+    if prompt!='':
+        response=model.generate_content([prompt,image])
+    else:
+        response=model.generate_content([prompt])
+    return response.text
+
+#Run the Function
+submit =st.button('Submit')
+if submit:
+    response=get_llm_response(prompt,image)
+    st.subheader(':orange[Response]')
+    st.markdown(response)
